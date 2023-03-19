19b517c0541f0de50d4be02992c409520cb226f3
napapon-wongkom <dot.wongkom@gmail.com>
2023-03-19 17:07:10 +0700

    lab25_3.cpp

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
 //Write function count() here.
+int count(int _data[],int n){
+ 	set<int> my_set;
+ 	for(int i=0;i<n;i++){
+ 		my_set.insert(_data[i]);
+ 	}
+ 	return my_set.size();
+ }
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
